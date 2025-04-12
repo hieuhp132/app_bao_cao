2e8bf5876c0bca4e99ef68945a347199311f2e46
Viet Minh Hieu Dao <v.dao@stud.uni-hannover.de>
2025-04-12 19:07:37 +0200

    recreate backend folder

--- sales-reporting-system/src/main/java/com/company/sales_reporting_system/repository/StoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.company.sales_reporting_system.repository;
-
-import com.company.sales_reporting_system.model.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-    // Các phương thức truy vấn tùy chỉnh nếu cần
-}
