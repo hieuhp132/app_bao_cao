a6aafa043aeb4b678724c7af21a71de4d36464be
Tuan Anh Nguyen <tuan.anh.nguyen@stud.uni-hannover.de>
2025-04-15 15:34:50 +0200

    update api call

--- sales-reporting-frontend/app/(tabs)/_layout.tsx
+++ sales-reporting-frontend/app/(tabs)/_layout.tsx
@@ -1,43 +1,52 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import Feather from "@expo/vector-icons/Feather";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Tổng quan",
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={24} color="black" />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Thêm",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="add-circle" size={24} color="black" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="report-post"
+        options={{
+          title: "Báo cáo",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name="newspaper" size={24} color="black" />
+          ),
         }}
       />
     </Tabs>
--- sales-reporting-frontend/app/(tabs)/index.tsx
+++ sales-reporting-frontend/app/(tabs)/index.tsx
@@ -1,74 +1,143 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Image, StyleSheet, Platform } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import axios from "axios";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function HomeScreen() {
+  const [jsonData, setData] = useState([]);
+  const round = (value: number, decimals: number): number => {
+    return Number(Math.round(Number(value + "e" + decimals)) + "e-" + decimals);
+  };
+  useEffect(() => {
+    const fetchReport = async () => {
+      try {
+        const res = await axios.get("http://localhost:8081/top-city");
+        setData(res.data);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchReport();
+  }, []);
+  console.log("data:", jsonData);
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.bottomDiv}>
+      <View style={styles.nav}>
+        <Text style={styles.navText}>Dashboard</Text>
+        <TouchableOpacity>
+          <Ionicons
+            name="notifications"
+            size={24}
+            color="white"
+            style={styles.iconNoti}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.BodyDiv}>
+        <View style={styles.rowfunc1}>
+          <Text style={styles.bodyText}>Tên người dùng : </Text>
+        </View>
+        <View style={styles.rowfunc}>
+          <View style={styles.money_earn}>
+            <MaterialIcons name="attach-money" size={24} color="white" />
+          </View>
+          <View>
+            <Text style={styles.bodyText}>
+              Thành phố có số lượng doanh thu lớn nhất là{" "}
+              <Text>{jsonData[0]}</Text>
+            </Text>
+            <Text style={styles.bodyText}>{round(jsonData[1], 2)}$</Text>
+          </View>
+        </View>
+        <View style={styles.rowfunc}>
+          <View style={styles.money_earn}>
+            <MaterialIcons name="attach-money" size={24} color="white" />
+          </View>
+          <View>
+            <Text style={styles.bodyText}>Tổng doanh thu hôm nay</Text>
+            <Text style={styles.bodyText}>12.500.500 đ</Text>
+          </View>
+        </View>
+        <View style={styles.rowfunc}>
+          <View style={styles.money_earn}>
+            <MaterialIcons name="attach-money" size={24} color="white" />
+          </View>
+          <View>
+            <Text style={styles.bodyText}>Tổng doanh thu hôm nay</Text>
+            <Text style={styles.bodyText}>12.500.500 đ</Text>
+          </View>
+        </View>
+        <View style={styles.rowfunc}></View>
+        <View style={styles.rowfunc}></View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  rowfunc1: {
+    marginTop: 20,
+    marginBottom: 10,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  money_earn: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    backgroundColor: "#2980b9",
+    marginLeft: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  rowfunc: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    height: "auto",
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 20,
+  },
+  bodyText: {
+    color: "black",
+    fontSize: 20,
+    fontFamily: "Roboto",
+  },
+  iconNoti: {
+    width: 24,
+  },
+  nav: {
+    marginTop: 40,
+    flexDirection: "row", // Align items horizontally
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+  navText: {
+    fontSize: 35,
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    color: "white",
+  },
+  bottomDiv: {
+    backgroundColor: "#2980b9",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingBottom: 10,
+    height: "100%",
+    flex: 1,
+  },
+  BodyDiv: {
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    backgroundColor: "#ecf0f1",
+    height: "100%",
+    flexDirection: "column",
+    overflow: "scroll",
+    paddingRight: 30,
+    paddingLeft: 30,
   },
 });
